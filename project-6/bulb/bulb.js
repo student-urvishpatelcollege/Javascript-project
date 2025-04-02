1bed7ec4dc7e25fb6ecf6d705f9c95faf1c8015f
Urvish Patel <147590992+student-urvishpatelcollege@users.noreply.github.com>
2025-04-02 18:40:26 +0530

    Add files via upload

---
+++ project-6/bulb/bulb.js
@@ -0,0 +1,9 @@
+let off = () => {
+    let image = document.getElementById("image")
+    image.src = "https://i.postimg.cc/KjK1wL3c/bulb-off.png";
+}
+
+let on = () => {
+    let image = document.getElementById("image");
+    image.src = "https://i.postimg.cc/6QyTynzr/bulb-on.png";
+}
---
+++ project-6/hover/hover.js
@@ -0,0 +1,9 @@
+let hover = () => {
+    let element = document.getElementById("hover");
+    element.innerText = "Thank You!!";
+}
+
+let leave = () => {
+    let element = document.getElementById("hover");
+    element.innerText = "hover!!";
+}
