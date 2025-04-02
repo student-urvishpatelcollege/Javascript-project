4323a24a30df3a6423b147d11925d401238ab6e7
Urvish Patel <147590992+student-urvishpatelcollege@users.noreply.github.com>
2025-04-02 18:27:06 +0530

    Add files via upload

---
+++ project-4/red-green-blue-theme/light.js
@@ -0,0 +1,12 @@
+function red() {
+    document.getElementById('box').style.backgroundColor = 'red';
+    document.getElementById('box').style.color = 'white';
+}
+function green() {
+    document.getElementById('box').style.backgroundColor = 'green';
+    document.getElementById('box').style.color = 'white';
+}
+function blue() {
+    document.getElementById('box').style.backgroundColor = 'blue';
+    document.getElementById('box').style.color = 'white';
+}
