91bf56380f0f705dbfcda6b3692527ed300c6e02
Urvish Patel <147590992+student-urvishpatelcollege@users.noreply.github.com>
2025-04-14 11:24:15 +0530

    Add files via upload

---
+++ project-8/slider.js
@@ -0,0 +1,26 @@
+const img_slider = document.querySelectorAll('.slide')
+let temp = 0;
+img_slider.forEach((img, ind) => {
+    img.style.left = `${ind * 100}%`
+})
+
+const slider = () => {
+    img_slider.forEach((item) => {
+        item.style.transform = `translateX(-${temp * 100}%)`
+    })
+}
+
+const prev = () => {
+    temp = (temp - 1 + img_slider.length) % img_slider.length;
+    slider();
+}
+
+const next = () => {
+    temp = (temp + 1) % img_slider.length;
+    slider();
+}
+
+setInterval(() => {
+    temp = (temp + 1) % img_slider.length;
+    slider();
+},5000);
